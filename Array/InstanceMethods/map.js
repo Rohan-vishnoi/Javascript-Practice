6c28413a692fb7f6fd27e8f715f4813ce79d1b07
rohan-vishnoi <avirohanvishnoi@gmail.com>
2025-08-25 21:34:21 +0530

    pop , push , reduce

--- Array/InstanceMethods/map.js
+++ Array/InstanceMethods/map.js
@@ -1,2 +1,26 @@
-Array.prototype.map()
+// Array.prototype.map()
+
+// The map() method of Array instances creates a new array populated
+// with the results of calling a provided function on every element
+// in the calling array.
+//
+// const array = [1, 4 , 9 , 16 , 23];
+//
+// const mapped = array.map(x => x * x);
+//
+// console.log(mapped);
+//
+// map(callbackfn)
+// map(callbackfn , thisArg);
+
+// The map method is an iterative method. it calls a provided callbackfn function once for each element in the array and constructs  a new array
+// from resukts
+
+// const num = [1,4,9];
+// console.log(num.map(num => Math.sqrt(num)));
+
+// Array.prototype.pop()
+
+// The pop method of Array instances remvoes form the last element from an array and returns element
+
 
