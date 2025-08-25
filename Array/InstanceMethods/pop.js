4b1925dd736bab55b74383bfede9b88c74a8d8ef
rohan-vishnoi <avirohanvishnoi@gmail.com>
2025-08-25 21:34:45 +0530

    pop , push , reduce

---
+++ Array/InstanceMethods/pop.js
@@ -0,0 +1,5 @@
+// The pop method of Array. instances removes the last element from an array
+
+const plants = ["brocolli" ,"cauliflower" , "cabbage"];
+
+console.log(plants.pop());
---
+++ Array/InstanceMethods/push.js
@@ -0,0 +1,8 @@
+// The push method of array instances add the specified elements to the end of the array
+
+// const animals = ["pig" ,"goats", "sheep"];
+// animals.push("cow");
+// console.log(animals);
+
+// The push method is mutating method
+
